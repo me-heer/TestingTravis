6c0e7087d25bff2be937721446bed9dd8f02421a
Mihir Joshi <mihir67mj@gmail.com>
2019-11-22 12:42:08 +0530

    Add files via upload

---
+++ src/main/java/com/App.java
@@ -0,0 +1,19 @@
+package com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static boolean testFunction(String testString){
+        if (testString.equals("Mihir"))
+            return true;
+        else 
+            return false;
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
